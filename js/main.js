196020c8f941dceba97734c6346f44d0eaee67ff
YoungRong.Oh <youngrong.oh@gmail.com>
2020-12-27 00:29:51 +0900

    JS: the Effect Turning over slide banner automatically

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+function setSlideInterval() {
+  const slideBanner = document.querySelector('.slide-banner__wrapper');
+  const slides = slideBanner.children;
+  let i = 0;
+  setInterval(() => {
+    if (i + 1 < slides.length) {
+      slideBanner.style.transform = `translateX(-${100 * (i + 1)}%)`;
+      i++;
+    } else {
+      slideBanner.style.transform = `translateX(0%)`;
+      i = 0;
+    }
+  }, 8000);
+}
+
 function AddSearchMode(navbar) {
   if (!navbar.classList.contains('search-mode')) {
     navbar.classList.add('search-mode');
@@ -29,3 +44,4 @@ function setSearchMode() {
 }
 
 setSearchMode();
+setSlideInterval();
